a32da3f1bfc2d31bf13f19564e059a58a1826a61
BhavaniThangavel <bhavanithangavel1499@gmail.com>
2024-07-11 09:01:58 +0530

    dial check

--- controller.js
+++ controller.js
@@ -1,32 +1,32 @@
 
 
-function verifySession(){
-    let user = localStorage.getItem('USER') || "";
-    if(user == ""){
-      alert("Session Expired/ Not logged In.. Please login again");
-      window.location.href = "./index.html";
-    }
-  }
+// function verifySession(){
+//     let user = localStorage.getItem('USER') || "";
+//     if(user == ""){
+//       alert("Session Expired/ Not logged In.. Please login again");
+//       window.location.href = "./index.html";
+//     }
+//   }
 
-class UserSession{
-    checkLogin(uName, pswd){
-        // let uName = document.getElementById('uName').value;
-        // let pswd = document.getElementById('pswd').value;
+// class UserSession{
+//     checkLogin(uName, pswd){
+//         // let uName = document.getElementById('uName').value;
+//         // let pswd = document.getElementById('pswd').value;
 
-        if(uName == 'bhavani@gmail.com' && pswd == 'pass123'){
-            sessionStorage.setItem('USER', uName);
-            window.location.href = './rrapp_homepage.html';
-        }else{
-          sessionStorage.removeItem('USER');
-            alert('Invalid Username/Password');
-        }
-    }
+//         if(uName == 'bhavani@gmail.com' && pswd == 'pass123'){
+//             sessionStorage.setItem('USER', uName);
+//             window.location.href = './rrapp_homepage.html';
+//         }else{
+//           sessionStorage.removeItem('USER');
+//             alert('Invalid Username/Password');
+//         }
+//     }
 
-    verifySession(){
-        let user = sessionStorage.getItem('USER') || "";
-        if(user == ""){
-          alert("Session Expired/ Not logged In.. Please login again");
-          window.location.href = "./index.html";
-        }
-      }
-}
+    // verifySession(){
+    //     let user = sessionStorage.getItem('USER') || "";
+    //     if(user == ""){
+    //       alert("Session Expired/ Not logged In.. Please login again");
+    //       window.location.href = "./index.html";
+    //     }
+    //   }
+// }
